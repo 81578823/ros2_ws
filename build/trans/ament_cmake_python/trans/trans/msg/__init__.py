c5afa6f65fc98bc522d7c9b9963790e74e7f25a1
81578823 <81578823@qq.com>
2025-02-19 20:11:16 +0800

    update

--- build/trans/ament_cmake_python/trans/trans/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from trans.msg._actuator_cmds import ActuatorCmds  # noqa: F401
-from trans.msg._touch_sensor import TouchSensor  # noqa: F401
--- build/trans/ament_cmake_python/trans/trans/srv/__init__.py
+++
@@ -1,2 +0,0 @@
-from trans.srv._gait_switch import GaitSwitch  # noqa: F401
-from trans.srv._simulation_reset import SimulationReset  # noqa: F401
